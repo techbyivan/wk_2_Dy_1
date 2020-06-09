de0656d4f2e1efa6d58f441b28bd30ae57e4e1df
Ivan Davis <techbyivan@users.noreply.github.com>
2020-06-09 09:38:24 -0400

    Cleaned Up Code

--- day_1_/medium_excer.py
+++ day_1_/medium_excer.py
@@ -1,4 +1,5 @@
-# 1. Find the smallest number
+# 1. 
+# Find the smallest number
 # Write a function smallest that accepts a List of numbers as an argument.
 # It should return the smallest number in the List.
 
@@ -12,7 +13,8 @@
 
 ################################################################################################################################################
 
-# #2. Find the largest number
+# #2. 
+# Find the largest number
 # Write a function largest that accepts a List of numbers as an argument.
 # It should return the largest number in the List.
 
